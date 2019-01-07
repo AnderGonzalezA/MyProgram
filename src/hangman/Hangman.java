b7fdbcebf4d8001dea0f518015a282c35c41acc2
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2019-01-07 09:21:47 +0100

    0701

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -2,7 +2,7 @@ package hangman;
 
 import java.util.Scanner;
 
-public class Main {
+public class Hangman {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		// Print the games' description
